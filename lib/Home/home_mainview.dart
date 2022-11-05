b2133966ca541a5ef743cab1707726d8b1ef4e88
whaeundo <1916388@sookmyung.ac.kr>
2022-11-05 17:56:48 +0900

    feat: #1 탭 바 세팅

---
+++ lib/Home/home_mainview.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeMainPage extends StatefulWidget {
+  const HomeMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<HomeMainPage> createState() => _HomeMainPageState();
+}
+
+class _HomeMainPageState extends State<HomeMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
