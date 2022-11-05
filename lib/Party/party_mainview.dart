c326ac7b5e61caa157711185561e93828b914913
whaeundo <1916388@sookmyung.ac.kr>
2022-11-05 23:17:20 +0900

    fix: #1 탭바 변경

---
+++ lib/Party/party_mainview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PartyMainPage extends StatefulWidget {
+  const PartyMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<PartyMainPage> createState() => _PartyMainPageState();
+}
+
+class _PartyMainPageState extends State<PartyMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Rental/rental_mainview.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RentalMainPage extends StatefulWidget {
+  const RentalMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<RentalMainPage> createState() => _RentalMainPageState();
+}
+
+class _RentalMainPageState extends State<RentalMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
